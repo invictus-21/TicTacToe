6fbf365e63f4a84654d7fb323dde716dcb9fb057
fotino21 <ataturhan21@gmail.com>
2024-03-16 19:42:09 -0500

    feature: buttons added

--- tictactoe/src/main/java/com/fx/tictactoejavafx/TicTacToe.java
+++ tictactoe/src/main/java/com/fx/tictactoejavafx/TicTacToe.java
@@ -14,14 +14,14 @@ import javafx.stage.Stage;
 
 
 public class TicTacToe extends Application {
-
     private final GridPane gridPane = new GridPane();
     private final BorderPane borderPane = new BorderPane();
     private final Label title = new Label("Tic Tac Toe");
 
+    private final Button[] btns = new Button[9];
+
     @Override
     public void start(Stage stage) {
-
         this.createGUI();
         Scene scene = new Scene(borderPane, 550, 650);
         stage.setTitle("Tic Tac Toe");
@@ -36,8 +36,22 @@ public class TicTacToe extends Application {
         borderPane.setTop(title);
         BorderPane.setAlignment(title, Pos.CENTER);
         borderPane.setPadding(new Insets(25, 25, 25, 25));
-    }
 
+        int label = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                Button button = new Button();
+                button.setId(String.valueOf(label));
+                button.setFont(font);
+                button.setPrefHeight(150);
+                button.setPrefWidth(150);
+                gridPane.add(button, j, i);
+                gridPane.setAlignment(Pos.CENTER);
+                btns[label++] = button;
+            }
+        }
+        borderPane.setCenter(gridPane);
+    }
     public static void main(String[] args) {
         launch(args);
     }
